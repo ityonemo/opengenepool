33dfd4f6b3dfb812c0ece3f0034cefbc5cc2380a
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2012-04-04 01:43:06 -0700

    imported code from selection javascript, first stage highlighting. 	modified:   public/editor/plugin_find.css 	modified:   public/editor/plugin_find.js

--- public/editor/plugin_find.js
+++ public/editor/plugin_find.js
@@ -4,7 +4,6 @@ var find = new Plugin("find");
 // MEMBER VARIABLES
 
 find.searchstring = "";
-find.searcharray = [];
 find.domain = {};
 
 ////////////////////////////////////////////////////////////////////////
@@ -16,7 +15,8 @@ find.initialize = function()
   find.toolbardom.innerHTML += "<input type='text' id='findinput'></input>" +
   "<button>&lt;&lt;</button><button>&gt;&gt;</button>";
   document.getElementById("findinput").addEventListener("keyup", find.find);
-  find.toolbardom.onclick = find.show();
+  //find.toolbardom.onclick = find.show();
+  find.domain = new Domain();
 }
 
 ////////////////////////////////////////////////////////////////////////
@@ -26,9 +26,13 @@ find.find = function()
 {
   //reset the search value and clear the search array.
   find.searchstring = document.getElementById("findinput").value;
-  find.searcharray = [];
   var searchrc = reversecomplement(find.searchstring);
   var searchlength = find.searchstring.length;
+  if (find.domain.ranges.length)
+  {
+    find.clear();
+    find.domain.ranges = [];
+  }
   if (searchlength > 2)
   {
     var terminus = editor.sequence.length - searchlength;
@@ -36,15 +40,89 @@ find.find = function()
     {
       var slicedstring = editor.sequence.substr(i, searchlength);
       if (slicedstring == find.searchstring)
-        find.searcharray.push(i)
+        find.domain.push(new FindRange(i, i+searchlength, 1))
       else if (slicedstring == searchrc)
-        find.searcharray.push(-i)
+        find.domain.push(new FindRange(i, i+searchlength, -1))
     }
+    find.paint();
   }
 }
 
-find.show = function()
+find.clear = function()
 {
+  for (var i = 0; i < find.domain.ranges.length; i++)
+  {
+    find.domain.ranges[i].remove();
+  }
+}
+
+find.paint = function()
+{
+  for (var i = 0; i < find.domain.ranges.length; i++)
+  {
+    find.domain.ranges[i].draw();
+  }
+}
+
+find.drawrange = function(path, range)
+{
+  var sel_span = range.span();
+  if (sel_span.start_s == sel_span.end_s)
+  {
+    var xpos = sel_span.start_p * graphics.metrics.charwidth;
+    var height = graphics.lines[sel_span.start_s].content.getBBox().height; 
+    path.attr("d",
+      "M " + xpos + "," + (graphics.lines[sel_span.start_s].translatey - height) +
+      " H " + (sel_span.end_p) * graphics.metrics.charwidth +
+      " v " + height  +
+      " H " + xpos +
+      " Z" 
+    );
+  }
+  else
+  {
+    var xpos1 = sel_span.start_p * graphics.metrics.charwidth;
+    var xpos2 = sel_span.end_p * graphics.metrics.charwidth;
+    var ypos1 = graphics.lines[sel_span.start_s].translatey;
+    var ypos2 = graphics.lines[sel_span.end_s].translatey;
+    var height1 = graphics.lines[sel_span.start_s].content.getBBox().height; 
+    var height2 = graphics.lines[sel_span.end_s].content.getBBox().height;
+
+    path.attr("d",
+    "M " + xpos1 + "," + (ypos1 - height1) +
+    " H " + graphics.metrics.linewidth +
+    " V " + (ypos2 - height2) +
+    " H " + (sel_span.end_p) * graphics.metrics.charwidth +
+    " v " + height2  +
+    " H " + 0 +
+    " V " + ypos1 +
+    " H " + xpos1 +
+    " Z"
+    );
+  }
+  path.transform("T" + graphics.settings.lmargin + ",0");
+  path.toBack();
+}
+
+FindRange = function(start, end, orientation)
+{
+  var range = new Range(start, end, orientation)
+  range.path = {};
+  
+  range.remove = function()
+  {
+    range.path.remove();
+  }
+
+  range.draw = function()
+  {
+    var newpath = graphics.editor.paper.path();
+    newpath.attr("class","found");
+    range.path = newpath;
+    find.drawrange(newpath, range);
+  }
+
+  return range;
 }
 
 
