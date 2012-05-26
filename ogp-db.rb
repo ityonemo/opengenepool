13582057a4f3ad722b7712ec207a4c274aa847f1
Isaac Yonemoto <isaac@indysci.org>
2012-05-26 02:41:41 -0700

    modified:   ogp-db.rb

--- ogp-db.rb
+++ ogp-db.rb
@@ -1,6 +1,6 @@
 $dblogin = ENV['OPENSHIFT_DB_USERNAME']
 $dbpass = ENV['OPENSHIFT_DB_PASSWORD']
-$dbhost = "127.4.36.129:3306"
+$dbhost = ENV['OPENSHIFT_DB_HOST']
 $dbname = ENV['OPENSHIFT_DB_USERNAME']
 
 def db_connect
