88d9bec3110ed33370d40b3d4ef1458c002c3018
Isaac Yonemoto <isaac@indysci.org>
2012-05-26 02:52:30 -0700

    modified:   ogp-db.rb

--- ogp-db.rb
+++ ogp-db.rb
@@ -1,8 +1,9 @@
 $dblogin = ENV['OPENSHIFT_DB_USERNAME']
 $dbpass = ENV['OPENSHIFT_DB_PASSWORD']
 $dbhost = ENV['OPENSHIFT_DB_HOST']
+$dbport = ENV['OPENSHIFT_DB_PORT']
 $dbname = ENV['OPENSHIFT_APP_NAME']
 
 def db_connect
-  $DB=Sequel.mysql($dbname, :user => $dblogin, :password => $dbpass, :host => $dbhost)
+  $DB=Sequel.mysql($dbname, :user => $dblogin, :password => $dbpass, :host => $dbhost, :port => $dbport)
 end
